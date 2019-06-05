15e3c5fe0060d33dd0b695dc8fc231eb71b08490
mikebrusilovsky <mbrusilovsky@architech.ca>
2019-06-05 19:00:10 +0200

    empty build

--- database/test-db.js
+++ database/test-db.js
@@ -1,8 +1,8 @@
-// const mongoose = require('mongoose');
-// const config = require('../config/config');
-//
-// const mongoDB = config.db.mongoTestUrl;
-// mongoose.connect(mongoDB, { useNewUrlParser: true });
-// mongoose.Promise = global.Promise;
-// const db = mongoose.connection;
-// db.on('error', console.morror.bind(console, 'MongoDB connection error:'));
+const mongoose = require('mongoose');
+const config = require('../config/config');
+
+const mongoDB = config.db.mongoTestUrl;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.morror.bind(console, 'MongoDB connection error:'));
